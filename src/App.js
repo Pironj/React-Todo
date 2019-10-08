c18c0ff81ad4edacb7bd4b277ffe20b0ab74ac61
Julian Piron <pironj27@gmail.com>
2019-10-08 10:38:42 -0700

    testing addItem and handleInput functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import TodoList from './components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <TodoList addItem={this.addItem}/>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      items: [],
+      currentItem: {text:'', key:''},
+    }
+  }
+    
+  handleInput = e => {
+    console.log('Input received');
+  }
+  addItem = () => {
+    console.log('You added an item!');
+  }
+  render() {
+    return (
+      <div className="App">
+        <TodoList 
+          addItem={this.addItem}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
