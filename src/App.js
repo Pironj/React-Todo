f3a315736092c54f20a3d6f7f3cd387e68d4722c
Julian Piron <pironj27@gmail.com>
2019-10-09 11:31:57 -0700

    updating comments on code helping explain thought process

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ class App extends Component {
     super()
 
     this.state = {
+      // array thaat stores each currentItem when add task button is clicked.
       items: [],
+      // create an object to hold our user input when entered and added to our task list
       currentItem: {
         text:'', 
         key:''
       },
     }
   }
-    
+  // function that handles key presses
   handleInput = e => {
     console.log('Input received');
   }
+  // function to handle when we click our add task button
   addItem = e => {
     e.preventDefault();
     console.log('You added an item!');
@@ -28,9 +31,13 @@ class App extends Component {
     return (
       <div className="App">
         <TodoList 
+          // porperties we add to our todolist component to handle different events
           addItem={this.addItem}
+          // passing our handleInput function here as a prop to our todolist component
           handleInput={this.handleInput}
+          // passing our ref to the input field declared above as a prop to todolist component
           inputElement={this.inputElement}
+          // passing the state of currentItem as a prop to our todolist component. Will display the value of the state once its set.
           currentItem={this.state.currentItem}
           />
       </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
-
+  // lifecycle method called to update form upon submit
   componentDidUpdate() {
     this.props.inputElement.current.focus()
   }
@@ -10,11 +10,15 @@ class TodoList extends Component {
     return (
       <div className="todoListMain">
         <div className="header">
+          {/* form which has our addItem function from our App.js file passed as prop to onSubmit */}
           <form onSubmit={this.props.addItem}>
             <input 
               placeholder="Task"
+              // a reference prop to the input our user enters for the task 
               ref={this.props.inputElement}
+              // prop that holds the text typed into the input field
               value={this.props.currentItem.text}
+              // function that tracks keystokes and calls our handle input function in react
               onChange={this.props.handleInput}
             />
             <button type="submit"> Add Task </button>
